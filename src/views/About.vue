839988f0ff2807e64237fb2c4e0cd09184fe719d
maevaI2! <maevaespana@orange.fr>
2020-09-25 18:56:58 +0200

    about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <br>
+    <p>This is a TP provided by the great Oulu Team made of Rémi Viotty and Maeva Espana, both students of the University of Rennes 1, France.</p>
+    <br>
+    <p>This TP was asked for the GLI module of the 2nd year of Masters ILA.</p>
   </div>
 </template>
