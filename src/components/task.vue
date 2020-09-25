ea376560bd2025cd0159bce312438a225ec5e7dd
maevaI2! <maevaespana@orange.fr>
2020-09-25 18:38:13 +0200

    edit button wip

--- src/components/task.vue
+++ src/components/task.vue
@@ -2,19 +2,23 @@
     <md-card>
       <md-card-header>
         <md-card-header-text>
-          <md-card-content>
+          <md-card-content v-if="!toggleNameEdit">
             {{task.name}}
           </md-card-content>
+          <md-field :class="{'md-invalid': nameTask.length == 0}" v-if="toggleNameEdit">
+            <md-textarea v-model="task.name"></md-textarea>
+            <span class="md-error">Enter your task here</span>
+          </md-field>
         </md-card-header-text>
+        <md-button class="md-fab md-mini md-plain" v-on:click="editTaskName()">
+          <md-icon>edit</md-icon>
+        </md-button>
       </md-card-header>
 
       <md-card-actions>
         <md-checkbox v-model="task.done">Done</md-checkbox>
       </md-card-actions>
     </md-card>
-    
-  
-
 </template>
 
 <script lang="ts">
@@ -27,7 +31,15 @@ import { Task } from "../models/task";
     task: Task,
   },
 })
-export default class TaskVue extends Vue {}
+export default class TaskVue extends Vue {
+
+  private toggleNameEdit = false;
+
+  editTaskName() {
+    this.toggleNameEdit = !this.toggleNameEdit;
+  }
+
+}
 </script>
 
 <style lang="css" scoped>
